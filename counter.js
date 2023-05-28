b64f714b88f33c56b98e42df08829467094f2aa9
betterkit <bettersoft@qq.com>
2023-05-28 14:54:37 +0800

    添加统计功能

---
+++ counter.js
@@ -0,0 +1,10 @@
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9994f86c93b26276db82895f78a55ae6";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
