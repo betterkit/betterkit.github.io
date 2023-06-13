fe208185cf6a052e8021c2c6594940fd8d4605a1
betterkit <bettersoft@qq.com>
2023-06-13 17:12:56 +0800

    www

--- load.js
+++ load.js
@@ -1,12 +1,3 @@
-
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?9994f86c93b26276db82895f78a55ae6";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 // loading ing
 window.onload = () => {
     var eles = document.querySelectorAll('.list-icon'); // 获取所有列表元素
