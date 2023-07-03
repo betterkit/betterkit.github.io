c546990535e0ecee9a8f724192826ec9aaef84ad
betterkit <bettersoft@qq.com>
2023-07-03 22:00:12 +0800

    55

--- footer.js
+++ footer.js
@@ -1,2 +1,2 @@
 // JavaScript Document
-document.write('<p style="float:left">© 2023 Bettersoft | <a href="mailto:bettersoft@qq.com">Contact US</a></p><p style="float: right;">oo</p>')
+document.write('<p style="float:left">© 2023 Bettersoft | <a href="mailto:bettersoft@qq.com">Contact US</a></p><p style="float:right;">Union</p>')
