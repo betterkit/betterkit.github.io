4906f6413f5a83c8699d5408b554ef4d1ad9b094
bettersoft <bettersoft@qq.com>
2023-12-22 22:10:08 +0800

    nav

--- navbar.js
+++ navbar.js
@@ -1,5 +1,6 @@
 document.write(`
     <ul class="cl">
+        <li><a href="chat.html" target="_self" title="首页">首页</a></li>
         <li><a href="chat.html" target="_self" title="AI对话">对话</a></li>
         <li><a href="draw.html" target="_self" title="AI绘画">绘画</a></li>
         <li><a href="write.html" target="_self" title="AI写作">写作</a></li>
@@ -9,7 +10,7 @@ document.write(`
         <li><a href="video.html" target="_self" title="AI视频">视频</a></li>
         <li><a href="audio.html" target="_self" title="AI音频">音频</a></li>
         <li><a href="code.html" target="_self" title="AI编程">编程</a></li>
-        <li><a href="3d.html" target="_self" title="3d">3 D</a></li>
+        <li><a href="3d.html" target="_self" title="3d三维">三维</a></li>
         <li><a href="prompt.html" target="_self" title="AI指令">指令</a></li>
         <li><a href="build.html" target="_self" title="AI开发">开发</a></li>
         <li><a href="business.html" target="_self" title="商务">商务</a></li>
