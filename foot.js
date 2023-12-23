a1b58bcba14592bfd10065c48679aecc6b6e91de
bettersoft <bettersoft@qq.com>
2023-12-23 19:27:54 +0800

    foot

---
+++ foot.js
@@ -0,0 +1,5 @@
+document.write(`
+        <p>推荐应用请发邮件至：<a href="mailto:bettersoft@qq.com">bettersoft@qq.com</a></p>
+        <p style="float:left">© 2023 Bettersoft</p>
+        <p style="float:right"><a href="hub.html">HUB</a></p>
+`)
