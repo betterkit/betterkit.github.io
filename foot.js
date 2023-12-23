130d7032f24fc59d008cc39da87424616f327659
bettersoft <bettersoft@qq.com>
2023-12-23 20:07:54 +0800

    bot

--- foot.js
+++ foot.js
@@ -1,5 +1,4 @@
 document.write(`
-        <p>推荐应用请发邮件至：<a href="mailto:bettersoft@qq.com">bettersoft@qq.com</a></p>
-        <p style="float:left">© 2023 Bettersoft</p>
-        <p style="float:right"><a href="hub.html">HUB</a></p>
+        <p>推荐应用请发邮件至 <a href="mailto:bettersoft@qq.com">bettersoft@qq.com</a></p>
+        <p>© 2023 Bettersoft.cn</p>
 `)
